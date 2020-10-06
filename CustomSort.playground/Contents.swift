9a7df9287ca4acfc09c88266d4045bb99ec6584c
Abhijeet Barge <abhijeet.barge@vuclip.com>
2020-10-06 10:14:29 +0530

    Custom sort in swift

---
+++ CustomSort.playground/Contents.swift
@@ -0,0 +1,24 @@
+
+func bubbleSort(array: inout Array<Int>)-> Array<Int>{
+    var done = false
+    print("Original - \(array)")
+    while !done {
+        done = true
+        print("Main iteration")
+        for i in 1..<array.count {
+            if (array.count > i) {
+                if array[i-1] > array[i] {
+                    done = false
+                    let tmp = array[i-1]
+                    array[i - 1] = array[i]
+                    array[i] = tmp
+                }
+            }
+            print(array)
+        }
+    }
+    return array
+}
+
+var testArr = [1,6,5,2,4]
+print("Final - \(bubbleSort(array: &testArr))")
